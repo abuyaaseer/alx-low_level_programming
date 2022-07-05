608b8da2ded0b82b9d69eed5774dcbbb1056f428
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-05 21:14:02 +0000

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,9 @@
 
 /**
 *_islower - to check for lower case
-*c - the parameter
-*Return: 0 or 1
 */
 int _islower(int c)
 {
-int lt = H;
-int c = h;
-if (int c == int lt)
-return (1)
-else
-return (0);
-
-
+	return (c >= 'z' && c <= 'z');
 
 }
