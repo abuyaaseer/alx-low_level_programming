41a32aa0aa926889b73c4068eb5c645810891db2
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-05 19:57:45 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints all alphabet x10
+*
+* return: nothing
+*/
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (j = 0; 0 < 10; j++)
+{
+for (i = 'a'; 'a' <= 'z'; i++)
+_putchar(i);
+}
+_putchar('\n');
+}
