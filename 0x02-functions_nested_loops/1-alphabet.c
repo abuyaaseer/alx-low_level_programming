cdbabc4f2e9cfa9cf34f6b1a88582fc72bfc0498
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-05 19:28:23 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - prints all alphabet
+*
+*/
+void print_alphabet(void)
+{
+int ch = 'a';
+
+while (ch < 'z')
+{
+	_putchar(ch);
+	ch++;
+}
+_putchar('\n');
+}
