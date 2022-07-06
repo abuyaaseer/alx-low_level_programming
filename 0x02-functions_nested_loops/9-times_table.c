20e39883bd71fbdd12feb04ed8deb4019b8578fd
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-06 17:13:37 +0000

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,10 +6,30 @@
 */
 void times_table(void)
 {
-	int b;
+	int x, y, k;
 
-	for (b = 0 ; 0 < 10 ; b++)
+	for (x = 0; x < 10; x++)
 	{
-		_putchar((b * 9) + '0');
+		for (y = 0; y < 10; y++)
+		{
+			k = x * y;
+			if (y == 0)
+				_putchar(k + 'o');
+			if (y != 0 && k < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
 	}
 }
