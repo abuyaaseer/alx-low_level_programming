d67d5498e024a89a1ee8bfc684cf3d81c245cb1d
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-06 16:46:44 +0000

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*times_table - prints multiplication table
+*Return: nothing
+*/
+void times_table(void)
+{
+	int b;
+
+	for (b = 0 ; 0 < 10 ; b++)
+	{
+		_putchar((b * 9) + '0');
+	}
+}
