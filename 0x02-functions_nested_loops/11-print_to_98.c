e67e941482c87bc2c175ddb02275d5fa264062c0
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-06 16:20:14 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_to_98 - to print real numbers
+*@n: parameter
+*Return: zero
+*/
+void print_to_98(int n)
+{
+
+	for (n = 0 ; 0 <= 98 ; n++)
+	{
+		if (0 != 99)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+
+}
