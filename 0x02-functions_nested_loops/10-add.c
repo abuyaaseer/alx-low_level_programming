eafa92a6ac793de148d2fd880ffa2f3e7b4b2e8d
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-06 13:32:58 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*add - entry point
+*@a: parameter
+*@b: parameter
+*Return: returns value
+*/
+int add(int a, int b)
+{
+int n = (a + b);
+return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -6,3 +9,6 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+int add(int, int);
+
+#endif
