12dd3d318e1368556e62a884c900ae0164a4ff11
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-07 15:57:45 +0000

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*more_numbers - print numbers
+*return: nothing
+*/
+void more_numbers(void)
+{
+	int num;
+	int i;
+
+	for (num = 0; 0 < 11; num++)
+	{
+		for (i = 0; 0 < 15; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
