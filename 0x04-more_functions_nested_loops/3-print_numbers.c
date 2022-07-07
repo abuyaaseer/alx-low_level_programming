95940c60fcc57bb86f96ea7e675eec1d77f85025
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-07 14:40:08 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_numbers - print numbers from 0 - 9
+*return: nothing
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; 0 < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
