d2c197abb08eaa286465343a52ede91c76e1d1c1
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-08 11:38:19 +0000

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,14 @@
 */
 void print_line(int n)
 {
-	int m = '_';
+	int x;
 
-	if (n > 0)
+	for (x = 0; x < n; x++)
 	{
-		_putchar(n * ('0' + m));
+		_putchar('_');
 		_putchar('\n');
 	}
-	else
+	if (n <= 0)
 	_putchar('\n');
 
 }
