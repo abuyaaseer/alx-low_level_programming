8292d98d82e79b4a70eeeb80576b4165661f4c18
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-08 22:50:53 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,28 +7,27 @@
 */
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("fizzBuzz");
+			printf(" fizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
-		if (i != 101)
-		printf(" ");
 	}
 	printf("\n");
 	return (0);
