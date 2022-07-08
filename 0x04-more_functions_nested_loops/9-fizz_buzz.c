c3a33bf1992b49f999af5e6f940de573e9c6a958
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-08 16:44:13 +0000

    fizz buzz challenge

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: zero
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		printf("fizzBuzz");
+
+		else if (i % 5 == 0)
+		printf("Buzz");
+
+		else if (i % 3 == 0)
+		printf("Fizz");
+
+		else
+		printf("%d", i);
+
+		if (i != 101)
+		printf(" ");
+	}
+	return (0);
+}
