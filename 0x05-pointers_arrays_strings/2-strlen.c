2f4d10697ffee0a7a8aa48bd814edffd687387ad
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-12 23:10:53 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_strlen - to check the lenth of a string
+*@s: parameter
+*Return: nothing
+*/
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+_putchar(i);
+}
