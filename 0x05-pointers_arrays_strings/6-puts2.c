1f2c11d7baebc0a7c7a5c4b1c5dd67c46e72057f
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-13 21:08:15 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*puts2 - to print even numbers
+*@str: parameter
+*Return: nothing
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+			break;
+		i++;
+	}
+	if ((i % 2) == 0)
+		_putchar(str[i]);
+	i++;
+}
