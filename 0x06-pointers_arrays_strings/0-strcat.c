8f7e6455334d10538e706de2155018573c82275e
Abdulkaree Bello <abdulkareembello93@gmail.com>
2022-07-14 17:24:50 +0000

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,20 @@
 */
 char *_strcat(char *dest, char *src)
 {
-strcat(dest, src);
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+	while (src[j] != 0)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] != 0;
 }
