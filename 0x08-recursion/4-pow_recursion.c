5993b617594c325c873c8b25cb8228016531b709
abuyaaseer <abdulkareembello93@gmail.com>
2022-07-20 19:06:11 +0000

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
 /**
 *_pow_recursion - to calculate raise to power using recursion
 *@x: parameter
-*@y"parameter
+*@y: parameter
 *Return: return -1 or raise to power value
 */
 int _pow_recursion(int x, int y)
@@ -13,6 +13,6 @@ int _pow_recursion(int x, int y)
 	else if (y == 0)
 		return (1);
 	else
-		return (x * _pow_recursion(x, y -1));
+		return (x * _pow_recursion(x, y - 1));
 
 }
