214fd8ed638c613b7fd49fcdc24700f6d84c18f4
abuyaaseer <abdulkareembello93@gmail.com>
2022-07-20 18:56:06 +0000

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_pow_recursion - to calculate raise to power using recursion
+*@x: parameter
+*@y"parameter
+*Return: return -1 or raise to power value
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x ^ _pow_recursion(x, y - 1));
+
+}
