f3fce94e6011315780bda37a7556f3e88ac439b2
abuyaaseer <abdulkareembello93@gmail.com>
2022-07-27 21:13:06 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints the number of arguments
+* @argc: number of command line argument
+* @argv: array that contain the programs in the command line argument
+* Return: returns
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv[0];
+	printf("%d", argc);
+
+	return (0);
+}
