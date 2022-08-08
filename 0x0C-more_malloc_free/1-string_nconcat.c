967a53077952a64f3850566352908654d7ff7943
abuyaaseer <abdulkareembello93@gmail.com>
2022-08-08 11:43:07 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,9 +31,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0; i < len1; i++)
-		s[i] = s1[i];
-	for (i = 0; i < len2; i++)
-		s[len1 + i] = s2[i];
+		if (i < len1)
+			s[i] = s1[i];
+		else
+			s[i] = s2[i - len1];
 	s[i] = '\0';
 
 	return (s);
