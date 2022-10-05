45cd158946fb1cc332a7fd04c8e223980b7f1e70
abuyaaseer <abdulkareembello93@gmail.com>
2022-10-05 20:06:58 +0000

    task 3 corrected

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,8 +15,9 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * height);
+	ptr = malloc(sizeof(int *) * height);
 	if (ptr == 0)
+		free(ptr);
 		return (NULL);
 
 	for (i = 0; i < height; i++)
