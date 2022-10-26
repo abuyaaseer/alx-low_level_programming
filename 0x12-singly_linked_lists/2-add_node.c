7cf85df5322e57c547e2bb760589722d91eb5f31
abuyaaseer <abdulkareembello93@gmail.com>
2022-10-26 15:43:18 +0000

    task 2 completed

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+*add_node - add a new node at the beginning
+*@head: parameter
+*@str: parameter
+*Return: returns head
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t count;
+
+	new = malloc(sizeof(list_t))
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+
+	for (count = 0; str[count]; count++)
+		;
+
+	new->len = count;
+	new->next = head;
+	head = new;
+	return (head);
+}
