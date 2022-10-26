0698157de00fd5dc6683f3e81c2a8bf8c7e2133c
abuyaaseer <abdulkareembello93@gmail.com>
2022-10-26 14:12:01 +0000

    changes in header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,14 +3,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
-*struct list_s - single linked list
-*@str: string
-*@len: length of string
-*@link: points to the next node
-*/
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @link: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX project
+ */
 typedef struct list_s
 {
 	char *str;
@@ -19,5 +22,9 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
