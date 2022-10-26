4ebf152579e6c7a7abc36f3904c04a2c5342cac9
abuyaaseer <abdulkareembello93@gmail.com>
2022-10-26 14:26:41 +0000

    changes in description of prototype

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,14 +2,14 @@
 
 /**
 *list_len - returns the number of elements in a linked list
-*@h - the head
+*@h: the head
 *Return: returns count
 */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		count++;
 		h = h->next;
