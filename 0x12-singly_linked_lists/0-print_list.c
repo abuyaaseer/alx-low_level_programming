139e613c40dd83ebdf181d50a71357f3570c830b
abuyaaseer <abdulkareembello93@gmail.com>
2022-10-26 13:44:40 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "list.h"
+
+/**
+*print_list - prints all element of a list
+*@h: head
+*Return: returns nothing
+*
+*/
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s", h->len, h->str);
+		count++;
+		h = h->link;
+	}
+}
---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,21 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+*struct list_s - single linked list
+*@str: string
+*@len: length of string
+*@link: points to the next node
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_t link;
+} list_t;
+
+#endif
